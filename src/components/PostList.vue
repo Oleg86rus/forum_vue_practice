176b3912d367dcebc3def2b5403d7a6fca97f92c
oleg086rus <o.pototskiy@yandex.ru>
2023-01-19 17:39:38 +0500

    decomposition

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="p in posts" class="post">
+    <div>
+      <strong>Название:</strong> {{ p.title }} </div>
+    <div>
+      <strong>Описание:</strong> {{ p.body }} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
